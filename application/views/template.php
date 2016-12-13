d249c345ecaf20aa5055a2d52c37965c26300d26
Calm <1calm2@gmail.com>
2016-12-13 16:13:58 +0300

    adaptive view

--- application/views/template.php
+++ application/views/template.php
@@ -1,33 +1,34 @@
 <!DOCTYPE html>
 <html>
 <head>
-    <meta charset="utf-8"/>
-    <link rel="shortcut icon" href="/application/templates/images/favicon.png" />
-    <link rel="stylesheet" type="text/css" href="/application/templates/css/common.css"/>
-    <?php $this->includeHeaders();?>
-    <title><?php echo $this->getTitle();?> — CGimpel</title>
+	<meta charset="utf-8"/>
+	<link rel="shortcut icon" href="/application/templates/images/favicon.png"/>
+	<link rel="stylesheet" type="text/css" href="/application/templates/css/common.css"/>
+	<?php $this->includeHeaders(); ?>
+	<title><?php echo $this->getTitle(); ?> — CGimpel</title>
 </head>
 <body>
 <div class="wrapper">
 
-    <div class="container">
-        <main class="main">
+	<div class="container">
+		<main class="main">
 
-            <?php $this->includePage();?>
+			<?php $this->includePage(); ?>
 
-        </main>
-    </div>
+		</main>
 
-    <aside class="left-sidebar">
-        <div class="menu">
-            <?php include 'parts/menu-top.php'; ?>
-            <div class="horizontal-separator">
-                <hr>
-            </div>
-            <?php $this->includeDynamicMenu();?>
-            <?php include 'control_panels/menu.php'; ?>
-        </div>
-    </aside>
+		<aside class="left-sidebar">
+			<div class="menu">
+				<?php include 'parts/menu-top.php'; ?>
+				<div class="horizontal-separator">
+					<hr>
+				</div>
+				<?php $this->includeDynamicMenu(); ?>
+				<?php include 'control_panels/menu.php'; ?>
+			</div>
+		</aside>
+
+	</div>
 
 </div>
 </body>
